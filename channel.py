f052862aed940f5586bbea63f5e58c7df5a1226f
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2024-02-28 17:10:40 +0100

    Add comment

--- channel.py
+++ channel.py
@@ -1,5 +1,5 @@
-class Channel:
-    def __init__(self, name, is_voice=False):
-        self.name = name
-        self.is_voice = is_voice
-        self.messages = []
+class Channel: # Define the Channel class
+    def __init__(self, name, is_voice=False): # Initialize the Channel with a name and whether it is a voice channel
+        self.name = name # The name of the channel
+        self.is_voice = is_voice # Whether the channel is a voice channel
+        self.messages = [] # A list of messages in the channel

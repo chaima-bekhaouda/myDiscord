d0a9dc65814c821c11274b00a6eb110eea7476f9
Chaima BEKHAOUDA <chaima.bekhaouda@laplateforme.io>
2024-03-06 22:44:30 +0100

    chore(cleanup): Remove unused files

--- channel.py
+++
@@ -1,5 +0,0 @@
-class Channel: # Define the Channel class
-    def __init__(self, name, is_voice=False): # Initialize the Channel with a name and whether it is a voice channel
-        self.name = name # The name of the channel
-        self.is_voice = is_voice # Whether the channel is a voice channel
-        self.messages = [] # A list of messages in the channel
